99d3d58f111c681aab9f6aefd2c3ccdb09bb1b8d
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-03 20:27:46 -0700

    Remove unused tasks.py

--- common/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from common.util.mailer import send_mail
-
-@shared_task(bind=True)
-def send_email(self, subject, emails, content):
-    """Sending email"""
-    send_mail(subject, emails, content)
-
-    return f"Email sent to: {emails}"
