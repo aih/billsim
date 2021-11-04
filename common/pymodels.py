d65eea7467839084fd1c9403e2843e00ca1c3b07
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-03 20:38:23 -0700

    Add Pydantic models

---
+++ common/pymodels.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+class Status(BaseModel):
+    success: bool
+    message: str 
+
+class BillPath(BaseModel):
+    billnumber_version: str 
+    path: str
+
+class SimilarSection(BaseModel):
+    from_section_id: str
+    to_section_id: str
+    from_section_label: str
+    to_section_label: str
+    from_section_header: str
+    to_section_header: str
+    score_es: float
+    score: float
+    score_other: float
+
+
+class SimilarBill(BaseModel):
+    id: int
+    billnumber_version: str
+    score: float
+    score_other: float
+    reason: str
+    billnumber_to: str
+    identified_by: str
+    title: str
+    similar_sections: list[SimilarSection]
+
+
