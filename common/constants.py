08c38ce1ba9b0882f0c147afd8dbe8d51d2c86c5
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-07 07:46:00 -0800

    Update constants and gitignore

--- common/constants.py
+++ common/constants.py
@@ -13,7 +13,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 CONGRESS_DATA_PATH = os.path.join(BASE_DIR, 'congress', 'data')
-BILLS_JSON_PATH = os.path.join(BASE_DIR, 'json_data') 
 
 PATH_BILLSECTIONS_JSON = os.path.join(
     BASE_DIR, 'es/billsections_mapping.json')
@@ -29,7 +28,6 @@ PATH_TO_DATA_DIR = os.getenv('PATH_TO_DATA_DIR', os.path.join('/', *"/usr/local/
 PATH_TO_CONGRESSDATA_XML_DIR = os.getenv('PATH_TO_CONGRESSDATA_XML_DIR', os.path.join('/', *"/usr/local/share/xcential/public/data/116/dtd".split('/')))
 PATH_TO_RELATEDBILLS_DIR = os.path.join(PATH_TO_CONGRESSDATA_DIR, 'relatedbills')
 
-USCONGRESS_XML_FILE = 'document.xml'
 
 RESULTS_DEFAULT = 20
 MIN_SCORE_DEFAULT = 25
@@ -62,11 +60,8 @@ BILL_DIR_REGEX_CDG_COMPILED = re.compile(BILL_DIR_REGEX_CDG)
 # congress/data/117/bills/sconres/sconres2
 US_CONGRESS_VERSION_PATH_REGEX_COMPILED = re.compile(r'data\/(?P<congress>[1-9][0-9]*)\/(?P<doctype>[a-z]+)\/(?P<stage>[a-z]{1,8})\/(?P<billnumber>[a-z]{1,8}[1-9][0-9]*)\/?(text-versions\/)?(?P<version>[a-z]+)')
 US_CONGRESS_PATH_REGEX_COMPILED = re.compile(r'data\/(?P<congress>[1-9][0-9]*)\/(?P<doctype>[a-z]+)\/(?P<stage>[a-z]{1,8})\/(?P<billnumber>[a-z]{1,8}[1-9][0-9]*)\/?(text-versions\/)?(?P<version>[a-z]+)?')
+USCONGRESS_XML_FILE = 'document.xml'
 
-BILL_TYPES = {
-  'ih': 'introduced',
-  'rh': 'reported to house'
-}
 
 # CDG = congress.gov
 def billNumberVersionFromPath_CDG(path: str):
@@ -135,6 +130,61 @@ CURRENT_CONGRESSIONAL_YEAR = datetime.date.today().year if datetime.date.today()
 START_CONGRESS = 110 # Earliest Congress with data in our database
 CURRENT_CONGRESS, cs_temp = divmod(round(((datetime.date(CURRENT_CONGRESSIONAL_YEAR, 1, 3) - datetime.date(1788, 1, 3)).days) / 365) + 1, 2)
 CURRENT_SESSION = cs_temp + 1
+
+BILL_TYPES ={ 
+'ih':  'IH-Introduced (House)',
+'as':  'AS-Amendment (Senate)',
+'ash': 'ASH-Additional Sponsors (House)',
+'ath': 'ATH-Agreed to (House)',
+'ats': 'ATS-Agreed to (Senate)',
+'cdh': 'CDH-Committee Discharged (House)',
+'cds': 'CDS-Committee Discharged (Senate)',
+'cph': 'CPH-Considered and Passed (House)',
+'cps': 'CPS-Considered and Passed (Senate)',
+'eah': 'EAH-Engrossed Amendment (House)',
+'eas': 'EAS-Engrossed Amendment (Senate)',
+'eh':  'EH-Engrossed (House)',
+'es':  'ES-Engrossed (Senate)',
+'enr': 'ENR-Enrolled',
+'eph': 'EPH-Engrossed and Deemed Passed by House',
+'fah': 'FAH-Failed Amendment (House)',
+'fph': 'FPH-Failed Passage (House)',
+'fps': 'FPS-Failed Passage (Senate)',
+'hdh': 'HDH-Held at Desk (House)',
+'hds': 'HDS-Held at Desk (Senate)',
+'iph': 'IPH-Indefinitely Postponed (House)',
+'ips': 'IPS-Indefinitely Postponed (Senate)',
+'is':  'IS-Introduced (Senate)',
+'lth': 'LTH-Laid on Table (House)',
+'lts': 'LTS-Laid on Table (Senate)',
+'oph': 'OPH-Ordered to be Printed (House)',
+'ops': 'OPS-Ordered to be Printed (Senate)',
+'pap': 'PAP-Printed as Passed',
+'pav': 'PAV-Previous Action Vitiated',
+'pch': 'PCH-Placed on Calendar (House)',
+'pcs': 'PCS-Placed on Calendar (Senate)',
+'pp':  'PP-Public Print',
+'pwah': 'PWAH-Ordered to be Printed with House Amendment',
+'rah':  'RAH-Referred with Amendments (House)',
+'ras':  'RAS-Referred with Amendments (Senate)',
+'rch':  'RCH-Reference Change (House)',
+'rcs':  'RCS-Reference Change (Senate)',
+'rdh':  'RDH-Received in (House)',
+'rds':  'RDS-Received in (Senate)',
+'reah': 'REAH-Re-engrossed Amendment (House)',
+'renr': 'RENR-Re-enrolled Bill',
+'res':  'RES-Re-engrossed Amendment (Senate)',
+'rfh':  'RFH-Referred in (House)',
+'rfs':  'RFS-Referred in (Senate)',
+'rh':  'RH-Reported in (House)',
+'rih': 'RIH-Referral Instructions (House)',
+'ris': 'RIS-Referral Instructions (Senate)',
+'rs':  'RS-Reported in (Senate)',
+'rth': 'RTH-Referred to Committee (House)',
+'rts': 'RTS-Referred to Committee (Senate)',
+'sas': 'SAS-Additional Sponsors (Senate)',
+'sc':  'SC-Sponsor Change'
+    }
 
 SAMPLE_QUERY = {
   "size": RESULTS_DEFAULT,
