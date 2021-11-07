a2b5baa8ff573aa52d10c8ff5477663bf72f7600
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-07 13:18:52 -0800

    Add test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -7,11 +7,19 @@ from billsim.pymodels import BillPath
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
-CONGRESS_DATA_PATH_TEST = os.path.join(BASE_DIR, 'samples', 'congress', 'data')
+CONGRESS_PATH_TEST = os.path.join(BASE_DIR, 'samples', 'congress')
+CONGRESS_DATA_PATH_TEST = os.path.join(CONGRESS_PATH_TEST, 'data')
 
 def test_walkBillDirs(rootDir = CONGRESS_DATA_PATH_TEST):
     from billsim.utils import walkBillDirs
     billDirs = walkBillDirs(rootDir=rootDir)
-    assert len(billDirs) == 27
+    assert len(billDirs) >= 27
+    assert billDirs[2].billnumber_version == "116hr2005ih"
+    assert billDirs[2].fileName == "BILLS-116hr2005ih.xml"
+
+def test_getBillXmlPaths(congressDir = CONGRESS_PATH_TEST):
+    from billsim.utils import getBillXmlPaths 
+    billDirs = getBillXmlPaths(congressDir=congressDir)
+    assert len(billDirs) >= 27
     assert billDirs[2].billnumber_version == "116hr2005ih"
     assert billDirs[2].fileName == "BILLS-116hr2005ih.xml"
