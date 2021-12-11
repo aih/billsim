594da7c5a2334f527b1d60d25c661be924f6fb56
aih <arihershowitz@gmail.com>
2021-12-10 16:51:22 -0800

    Update with comparematrix executable to get bill similarities

---
+++ src/billsim/compare.py
@@ -0,0 +1,22 @@
+import subprocess
+import json
+from billsim import pymodels
+from billsim.constants import COMPAREMATRIX_GO_CMD
+from billsim.utils import billNumberVersionToBillPath
+
+COMPAREMATRIX_GO_CMD = '../../bin/comparematrix'
+
+
+def getComparisonMatrix(billnumbers: list[str]) -> list[list]:
+    billPaths = [
+        billNumberVersionToBillPath(billnumber).filePath
+        for billnumber in billnumbers
+    ]
+    billPathsString = ",".join(billPaths)
+    result = subprocess.run(
+        [COMPAREMATRIX_GO_CMD, '-abspaths', billPathsString],
+        capture_output=True,
+        text=True)
+    comparematrix = json.loads(result.stdout.split(':compareMatrix:')[1])
+    # matrix of the form '[[{1 identical} {0.63 incorporates}] [{0.79 incorporated by} {1 identical}]]'
+    return comparematrix
