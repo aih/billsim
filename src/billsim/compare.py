603be92c840c425656fcd89e2027d09f72e9df80
aih <arihershowitz@gmail.com>
2021-12-11 23:03:55 -0800

    Add timer for compare

--- src/billsim/compare.py
+++ src/billsim/compare.py
@@ -1,11 +1,20 @@
+import sys
+import time
+import logging
 import subprocess
 import json
+import argparse
+import random
 from billsim import pymodels
 from billsim.constants import COMPAREMATRIX_GO_CMD
-from billsim.utils import billNumberVersionToBillPath
+from billsim.utils import billNumberVersionToBillPath, getBillXmlPaths, getBillnumberversionParts
 from billsim.bill_similarity import getSimilarBillSections, getBillToBill
 from billsim.utils_db import save_bill_to_bill, save_bill_to_bill_sections
 from billsim.pymodels import BillToBillModel
+
+logging.basicConfig(filename='compare.log', filemode='w', level='INFO')
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 def getCompareMatrix(billnumbers: list[str]) -> list[list]:
@@ -23,30 +32,69 @@ def getCompareMatrix(billnumbers: list[str]) -> list[list]:
     return comparematrix
 
 
-def processSimilarBills(billnumber_versions: list[str]) -> None:
-    # TODO: test that billnumber_versions is a list of billnumbers
-    for billnumber_version in billnumber_versions:
-        s = getSimilarBillSections(billnumber_version)
-        b2b = getBillToBill(s)
-        for bill in b2b:
-            save_bill_to_bill(b2b[bill])
-            save_bill_to_bill_sections(
-                b2b[bill]
-            )    # This should save the individual sections and the sections to section mapping
+def processSimilarBills(billnumber_version: str) -> list[str]:
+    try:
+        getBillnumberversionParts(billnumber_version)
+    except ValueError:
+        logger.error(
+            f'billnumber_version {billnumber_version} is not a valid billnumber_version'
+        )
+        return []
 
-        # Get similarity scores for bill-to-bill
-        # Calls comparematrix from bills (Golang);
-        similar_bills = b2b.keys()
-        c = getCompareMatrix(list(similar_bills))
-        for row in c:
-            for column in row:
-                compare_bill, compare_bill_to = column['ComparedDocs'].split(
-                    '-')
-                if compare_bill and compare_bill_to:
-                    b2bModel = BillToBillModel(
-                        billnumber_version=compare_bill,
-                        billnumber_version_to=compare_bill_to,
-                        score=column['Score'],
-                        score_to=column['ScoreOther'],
-                        reasons=[column['Explanation']])
-                    save_bill_to_bill(b2bModel)
+    s = getSimilarBillSections(billnumber_version)
+    b2b = getBillToBill(s)
+    for bill in b2b:
+        save_bill_to_bill(b2b[bill])
+        save_bill_to_bill_sections(b2b[bill])
+
+    # Get similarity scores for bill-to-bill
+    # Calls comparematrix from bills (Golang);
+    similar_bills = list(b2b.keys())
+    c = getCompareMatrix(similar_bills)
+    for row in c:
+        for column in row:
+            compare_bill, compare_bill_to = column['ComparedDocs'].split('-')
+            if compare_bill and compare_bill_to:
+                b2bModel = BillToBillModel(
+                    billnumber_version=compare_bill,
+                    billnumber_version_to=compare_bill_to,
+                    score=column['Score'],
+                    score_to=column['ScoreOther'],
+                    reasons=[column['Explanation']])
+                save_bill_to_bill(b2bModel)
+    return similar_bills
+
+
+def compareBills(maxBills: int = -1):
+    start_time = time.time()
+    billPaths = getBillXmlPaths()
+    if maxBills > 0:
+        billPaths = random.sample(billPaths, maxBills)
+        logger.info(f'Sampled {len(billPaths)} bills to process')
+    else:
+        maxBills = len(billPaths)
+    for billPath in billPaths:
+        try:
+            similar_bills = processSimilarBills(billPath.billnumber_version)
+            logger.debug(
+                f'{billPath.billnumber_version} has {len(similar_bills)} similar bills: {similar_bills}'
+            )
+        except Exception as e:
+            logger.error(
+                f'Error processing similarbills for bill {billPath.billnumber_version}'
+            )
+    end_time = time.time()
+    logger.info("It took {0} ms to process {1} bills.".format(
+        end_time - start_time, maxBills))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Get similarity scores for bills.')
+    parser.add_argument('max',
+                        metavar='m',
+                        type=int,
+                        help='max number of bills to compare')
+
+    args = parser.parse_args()
+    compareBills(maxBills=args.max)
--- src/billsim/database.py
+++ src/billsim/database.py
@@ -6,7 +6,7 @@ from sqlmodel import create_engine
 
 postgres_url = f"postgresql://postgres:{POSTGRES_PW}@localhost"
 
-engine = create_engine(postgres_url, echo=True)
+engine = create_engine(postgres_url, echo=False)
 
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
